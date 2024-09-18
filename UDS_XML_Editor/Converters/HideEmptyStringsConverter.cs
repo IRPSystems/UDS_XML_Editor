36d0efecacde83bf69b19eaca8f6b6e880194871
Smadar Tsdaka <smadar@irpsystems.com>
2024-09-18 08:45:47 +0300

    Add HideEmptyStringsConverter

---
+++ UDS_XML_Editor/Converters/HideEmptyStringsConverter.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UDS_XML_Editor.Converters
+{
+    public class HideEmptyStringsConverter : IValueConverter
+	{
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(value == null)
+				return Visibility.Collapsed;
+
+			if(!(value is string str))
+				return Visibility.Visible;
+
+			if(string.IsNullOrEmpty(str)) 
+				return Visibility.Collapsed;
+
+			return Visibility.Visible;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return string.Empty;
+		}
+	}
+}
