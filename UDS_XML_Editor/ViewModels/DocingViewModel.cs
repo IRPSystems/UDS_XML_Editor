295ea773368ae4e753da32ca50b8ff6485106d53
Smadar Tsdaka <smadar@irpsystems.com>
2024-09-18 10:29:07 +0300

    Add SystemView

--- UDS_XML_Editor/ViewModels/DocingViewModel.cs
+++ UDS_XML_Editor/ViewModels/DocingViewModel.cs
@@ -41,14 +41,17 @@ namespace UDS_XML_Editor.ViewModels
 		{
 			Children.Clear();
 
+			SystemView systemView = new SystemView() { DataContext = system };
+			CreateTabbedWindow(systemView, "System", string.Empty, out _appResponses);
+
 			MainElementView customersListView = new MainElementView() { DataContext = customersList };
-			CreateTabbedWindow(customersListView, "Customers", string.Empty, out _appCustormers);
+			CreateTabbedWindow(customersListView, "Customers", "System", out _appCustormers);
 
 			MainElementView requestsListView = new MainElementView() { DataContext = requestsList };
-			CreateTabbedWindow(requestsListView, "Requests", "Customers", out _appRequests);
+			CreateTabbedWindow(requestsListView, "Requests", "System", out _appRequests);
 
 			MainElementView responsesListView = new MainElementView() { DataContext = responsesList };
-			CreateTabbedWindow(responsesListView, "Responses", "Customers", out _appResponses);
+			CreateTabbedWindow(responsesListView, "Responses", "System", out _appResponses);
 
 		}
 
